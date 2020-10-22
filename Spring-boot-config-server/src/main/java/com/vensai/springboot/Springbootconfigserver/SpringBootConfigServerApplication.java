d94868c6ecd47644e123f7ba66590c8669c4b0ae
maniram <balijepalli93@gmail.com>
2020-10-22 16:15:57 +0530

    committing changes

--- Spring-boot-config-server/src/main/java/com/vensai/springboot/Springbootconfigserver/SpringBootConfigServerApplication.java
+++ Spring-boot-config-server/src/main/java/com/vensai/springboot/Springbootconfigserver/SpringBootConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.vensai.springboot.Springbootconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SpringBootConfigServerApplication {
 
