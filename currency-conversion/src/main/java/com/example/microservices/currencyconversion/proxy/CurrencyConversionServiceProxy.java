70cc789ffcbebbb420e017cf3fcdf740a2ae2c5c
maniram <balijepalli93@gmail.com>
2020-12-01 15:40:39 +0530

    committing changes

--- currency-conversion/src/main/java/com/example/microservices/currencyconversion/proxy/CurrencyConversionServiceProxy.java
+++ currency-conversion/src/main/java/com/example/microservices/currencyconversion/proxy/CurrencyConversionServiceProxy.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.example.microservices.currencyconversion.model.CurrencyConversion;
 
 //@FeignClient(name="currency-exchange-service",url="localhost:8000")
-@FeignClient(name="currency-exchange-service")
+//@FeignClient(name="currency-exchange-service")
+@FeignClient(name="zull-api")
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyConversionServiceProxy {
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversion getExchangeValues(@PathVariable("from") String from , @PathVariable("to") String to) ;
 }
---
+++ zull-api/src/test/java/com/example/microservices/zullapi/ZullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservices.zullapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
